72e90a092f79c6b981f5333fefe23d8849c71d4e
Jack Morris <jack@jackmorris.xyz>
2024-01-02 17:10:56 +0000

    Add SwiftDB package

--- Package.swift
+++ Package.swift
@@ -1,23 +1,30 @@
 // swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SwiftDB",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "SwiftDB",
-            targets: ["SwiftDB"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftDB"),
-        .testTarget(
-            name: "SwiftDBTests",
-            dependencies: ["SwiftDB"]),
-    ]
+  name: "SwiftDB",
+  platforms: [.iOS(.v17), .macOS(.v14)],
+  products: [
+    .library(
+      name: "SwiftDB",
+      targets: ["SwiftDB"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "SwiftDB"
+    ),
+    .testTarget(
+      name: "SwiftDBTests",
+      dependencies: ["SwiftDB"]
+    ),
+  ]
 )
+
+for target in package.targets {
+  var settings = target.swiftSettings ?? []
+  settings.append(.enableExperimentalFeature("StrictConcurrency"))
+  settings.append(.enableUpcomingFeature("ExistentialAny"))
+  target.swiftSettings = settings
+}
--- Tests/SwiftDBTests/SwiftDBTests.swift
+++ Tests/SwiftDBTests/SwiftDBTests.swift
@@ -1,12 +1,12 @@
-import XCTest
 @testable import SwiftDB
+import XCTest
 
 final class SwiftDBTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+  func testExample() throws {
+    // XCTest Documentation
+    // https://developer.apple.com/documentation/xctest
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+    // Defining Test Cases and Test Methods
+    // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+  }
 }
